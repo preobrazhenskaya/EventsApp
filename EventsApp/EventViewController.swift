29972ef6e0d9ada3250a87b36a67c2f03a019db1
Yana <yanak.preobrazhenskaya@yandex.ru>
2019-09-06 04:33:58 +0400

    Add drawing on Description screen

--- EventsApp/EventViewController.swift
+++ EventsApp/EventViewController.swift
@@ -22,11 +22,60 @@ class EventViewController: UIViewController {
     @IBOutlet weak var placeLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dashedView: UIView!
+    @IBOutlet weak var leftSemiCircle: UIView!
+    @IBOutlet weak var rightSemiCircle: UIView!
+    @IBOutlet weak var leftCircle: UIView!
+    @IBOutlet weak var rightCircle: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = event?.name
+        drawCircle(circle: leftCircle)
+        drawCircle(circle: rightCircle)
+        drawSemiCircle(circle: leftSemiCircle, center: CGPoint(x: 0, y: leftSemiCircle.bounds.maxY / 2))
+        drawSemiCircle(circle: rightSemiCircle, center: CGPoint(x: rightSemiCircle.bounds.maxX,
+                                                                y: rightSemiCircle.bounds.maxY / 2))
+        drawDottedLine(line: dashedView)
         setData()
+    }
+    
+    func drawCircle(circle : UIView) {
+        circle.backgroundColor = .clear
+        let circleLayer = CAShapeLayer()
+        let center = CGPoint(x: circle.bounds.maxX / 2, y: circle.bounds.maxY / 2)
+        let radius = circle.frame.size.width / 2
+        let circlePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 180,
+                                      clockwise: true)
+        circleLayer.path = circlePath.cgPath
+        circleLayer.strokeColor = UIColor.white.cgColor
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circle.layer.addSublayer(circleLayer)
+    }
+    
+    func drawSemiCircle(circle : UIView, center : CGPoint) {
+        circle.backgroundColor = .clear
+        let semiCircleLayer = CAShapeLayer()
+        let radius = circle.frame.size.width
+        let circlePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 90,
+                                      clockwise: true)
+        semiCircleLayer.path = circlePath.cgPath
+        semiCircleLayer.fillColor = UIColor(red: 247, green: 248, blue: 249, alpha: 1).cgColor
+        circle.layer.addSublayer(semiCircleLayer)
+    }
+    
+    func drawDottedLine(line: UIView) {
+        line.backgroundColor = .clear
+        let start = CGPoint(x: 0, y: 1)
+        let end = CGPoint(x: line.frame.maxX, y: 1)
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = UIColor.lightGray.cgColor
+        shapeLayer.lineWidth = 1
+        shapeLayer.lineDashPattern = [7, 3]
+        let path = CGMutablePath()
+        path.addLines(between: [start, end])
+        shapeLayer.path = path
+        line.layer.addSublayer(shapeLayer)
     }
     
     func setData() {
